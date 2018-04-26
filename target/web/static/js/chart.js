5fc97ace4ab8e14345e5788180a29e6fdebabda3
117110793 LUCAS CORDEIRO BRASIL <lucascb@lcc3-050.lcc.ufcg.edu.br>
2018-04-26 15:46:04 -0300

    Envio dos algoritmos implementados em aula

---
+++ target/web/static/js/chart.js
@@ -0,0 +1,108 @@
+// Wrapping in nv.addGraph allows for '0 timeout render', stores rendered charts in nv.graphs, and may do more in the future... it's NOT required
+    var chart;
+    var colors = ["#ff7f0e","#2ca02c","#2222ff","#667711","#EF9CFB"];
+	//var data = [{values: [{x:50,y:0.026843000203371048},{x:100,y:0.002764000091701746},{x:150,y:0.0035520000383257866},{x:200,y:0.002368000103160739}],key: "Bubble Sort",color: colors[0]},
+	//			{values: [{x:50,y:0.0027630000840872526},{x:100,y:0.0011840000515803695},{x:150,y:0.0015790000325068831},{x:200,y:0.001973999897018075}],key: "Selection Sort",color: colors[1]}];
+	
+	var data = [{values:[{x:10,y:23.818143844604492},{x:100,y:8.978857040405273},{x:500,y:17.62070655822754},{x:1000,y:22.00538444519043},{x:2000,y:64.04544067382812},{x:4000,y:271.068603515625},{x:6000,y:136.45423889160156},{x:10000,y:283.716064453125},{x:15000,y:616.7879028320312},{x:20000,y:1122.722412109375}],key:"BubbleSort",color:colors[0]},{values:[{x:10,y:0.7540540099143982},{x:100,y:1.0060739517211914},{x:500,y:8.786075592041016},{x:1000,y:11.341951370239258},{x:2000,y:12.467066764831543},{x:4000,y:23.280481338500977},{x:6000,y:49.602081298828125},{x:10000,y:126.29106140136719},{x:15000,y:267.8244934082031},{x:20000,y:469.0780944824219}],key:"SelectionSort",color:colors[1]},{values:[{x:10,y:0.5820220112800598},{x:100,y:0.9380130171775818},{x:500,y:6.195455074310303},{x:1000,y:10.355484008789062},{x:2000,y:58.67599105834961},{x:4000,y:48.90232467651367},{x:6000,y:119.84408569335938},{x:10000,y:304.9813537597656},{x:15000,y:726.5150146484375},{x:20000,y:1198.37353515625}],key:"InsertionSort",color:colors[2]}];;
+	
+	nv.addGraph(function() {
+        chart = nv.models.lineChart()
+            .options({
+                transitionDuration: 50
+            })
+			.useInteractiveGuideline(true)
+        ;
+		chart.interpolate('basis');
+		chart.useInteractiveGuideline(true)
+		
+        chart.xAxis
+            .axisLabel("Input size")
+            .tickFormat(d3.format(',.0f'))
+            //.staggerLabels(true)
+        ;
+
+        chart.yAxis
+            .axisLabel('Time (ms)')
+            .tickFormat(function(d) {
+                if (d == null) {
+                    return 'N/A';
+                }
+                return d3.format(',.0f')(d);
+            })
+        ;
+
+        d3.select('#chart1').append('svg')
+            .datum(data)
+            .call(chart);
+
+        nv.utils.windowResize(chart.update);
+
+        return chart;
+    });
+
+	
+    /* function sinAndCos() {
+        var sin = [],
+            sin2 = [],
+            cos = [],
+            rand = [],
+            rand2 = []
+            ;
+
+        for (var i = 0; i < 100; i++) {
+            sin.push({x: i, y: i % 10 == 5 ? null : Math.sin(i/10) }); //the nulls are to show how defined works
+            sin2.push({x: i, y: Math.sin(i/5) * 0.4 - 0.25});
+            cos.push({x: i, y: .5 * Math.cos(i/10)});
+            rand.push({x:i, y: Math.random() / 10});
+            rand2.push({x: i, y: Math.cos(i/10) + Math.random() / 10 })
+        }
+
+		var alg1 = []
+		alg1.push({x:0,y:0})
+		alg1.push({x:50,y:0.026843000203371048})
+		alg1.push({x:100,y:0.002764000091701746})
+		alg1.push({x:150,y:0.0035520000383257866})
+		alg1.push({x:150,y:0.002368000103160739})
+		
+		//[{"xaxis":200,"yaxis":,"algorithmCode":0,"algorithm":"BubbleSort"},{"xaxis":0,"yaxis":0.0,"algorithmCode":0,"algorithm":"BubbleSort"},{"xaxis":50,"yaxis":0.006711000110954046,"algorithmCode":0,"algorithm":"BubbleSort"},{"xaxis":100,"yaxis":0.002368000103160739,"algorithmCode":0,"algorithm":"BubbleSort"},{"xaxis":150,"yaxis":0.0015790000325068831,"algorithmCode":0,"algorithm":"BubbleSort"},{"xaxis":200,"yaxis":0.0015790000325068831,"algorithmCode":0,"algorithm":"BubbleSort"},{"xaxis":0,"yaxis":0.0,"algorithmCode":1,"algorithm":"SelectionSort"},{"xaxis":50,"yaxis":0.0027630000840872526,"algorithmCode":1,"algorithm":"SelectionSort"},{"xaxis":100,"yaxis":0.0011840000515803695,"algorithmCode":1,"algorithm":"SelectionSort"},{"xaxis":150,"yaxis":0.0015790000325068831,"algorithmCode":1,"algorithm":"SelectionSort"},{"xaxis":200,"yaxis":0.001973999897018075,"algorithmCode":1,"algorithm":"SelectionSort"},{"xaxis":0,"yaxis":0.0,"algorithmCode":0,"algorithm":"BubbleSort"},{"xaxis":50,"yaxis":0.008685000240802765,"algorithmCode":0,"algorithm":"BubbleSort"},{"xaxis":100,"yaxis":0.00355300004594028,"algorithmCode":0,"algorithm":"BubbleSort"},{"xaxis":150,"yaxis":0.002764000091701746,"algorithmCode":0,"algorithm":"BubbleSort"},{"xaxis":200,"yaxis":0.3687039911746979,"algorithmCode":0,"algorithm":"BubbleSort"},{"xaxis":0,"yaxis":0.0,"algorithmCode":1,"algorithm":"SelectionSort"},{"xaxis":50,"yaxis":0.01618500053882599,"algorithmCode":1,"algorithm":"SelectionSort"},{"xaxis":100,"yaxis":0.001973999897018075,"algorithmCode":1,"algorithm":"SelectionSort"},{"xaxis":150,"yaxis":0.0015790000325068831,"algorithmCode":1,"algorithm":"SelectionSort"},{"xaxis":200,"yaxis":0.0015780000248923898,"algorithmCode":1,"algorithm":"SelectionSort"},{"xaxis":0,"yaxis":0.0,"algorithmCode":2,"algorithm":"InsertionSort"},{"xaxis":50,"yaxis":0.0027630000840872526,"algorithmCode":2,"algorithm":"InsertionSort"},{"xaxis":100,"yaxis":0.0015790000325068831,"algorithmCode":2,"algorithm":"InsertionSort"},{"xaxis":150,"yaxis":0.0011840000515803695,"algorithmCode":2,"algorithm":"InsertionSort"},{"xaxis":200,"yaxis":0.0015790000325068831,"algorithmCode":2,"algorithm":"InsertionSort"}]
+        return [
+            //{
+            //    area: true,
+            //    values: sin,
+            //    key: "Sine Wave",
+            //    color: "#ff7f0e",
+            //    strokeWidth: 4,
+            //    classed: 'dashed'
+            //},
+            //{
+            //    values: cos,
+            //    key: "Cosine Wave",
+            //    color: "#2ca02c"
+            //},
+			{
+                values: alg1,
+                key: "Bubble Sort",
+                //color: "#2ca02c"
+				color: colors[0]
+            },
+            //{
+            //    values: rand,
+            //    key: "Random Points",
+            //    color: "#2222ff"
+            //},
+            //{
+            //    values: rand2,
+            //    key: "Random Cosine",
+            //    color: "#667711",
+            //    strokeWidth: 3.5
+            //},
+            //{
+            //    area: true,
+            //    values: sin2,
+            //    key: "Fill opacity",
+            //    color: "#EF9CFB",
+            //    fillOpacity: .1
+            //}
+        ];
+    } */
